8cd09514234994e2070f4ea07272c2e3042f471a
kelson42 <kelson42@users.sourceforge.net>
2011-09-19 13:57:46 +0000

    + improvement of the feedback system

---
+++ input.kiwix.org/post.php
@@ -0,0 +1,28 @@
+<?
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $country = geoip_country_name_by_name($ip) == "" ? "unknown country" : geoip_country_name_by_name($ip);
+    $input = $_POST["input"];
+    $message = $_POST["message"];
+    $version = $_POST["version"];
+    $browser = $_SERVER['HTTP_USER_AGENT'];
+
+    $to = "emmanuel@engelhart.org";
+    $from = "Kiwix Feedback System <feedback@kiwix.org>";
+    $subject = "Kiwix Feedback from $country ($ip)";
+    $headers = "From: $from\r\n";
+    $headers .= "Content-type: text/plain\r\n";
+
+    $content  = "MESSAGE\n==================================================\n";
+    $content .= "$message\n\n";
+    $content .= "\nADDITIONAL INFORMATIONS\n==================================================\n";
+    $content .= "Input:        $input\n";
+    $content .= "Version:      $version\n";	
+    $content .= "IP:           $ip\n";	
+    $content .= "Location:     $country\n";
+    $content .= "browser & OS: $browser\n";
+
+    mail($to, $subject, $content, $headers);
+
+    header("Location: /thanks.html");
+    exit();
+?> 
